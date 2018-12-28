171f732d48f20e9acf95e02a1ce936417ec6e343
Hiro Ayettey <ehayette@edu.uwaterloo.ca>
2018-12-27 20:45:36 -0500

    Initial Commit

---
+++ MathGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MathGame
+//
+//  Created by Hiro Ayettey on 2018-12-27.
+//  Copyright © 2018 Hiro Ayettey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
